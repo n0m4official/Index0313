3008033865aa4d773fa9f6d09a4f433492118787
n0m4official <ba55dragonofficial999@gmail.com>
2024-03-13 08:58:04 -0600

    Somewhat works now

--- scripts/console.py
+++ scripts/console.py
@@ -1,7 +1,9 @@
-txt = "abc"
+txt = input("Input text: ")
+def count_vowel(txt):
+    vowel_count = 0
+    for char in txt:
+        if char in ["a", "e", "i", "o", "u"]:
+            vowel_count += 1
+    return vowel_count
 
-#count the vowels in some text
-#in ABC, I should have one vowel
-
-print(txt.split())
-
+print(f"Number of vowels in {txt} is {count_vowel(txt)}")
