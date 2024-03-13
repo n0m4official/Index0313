82195b55a632c7a67f5b96d41f778ee9c877568a
n0m4official <ba55dragonofficial999@gmail.com>
2024-03-13 10:11:45 -0600

    testing

--- java/java.js
+++ java/java.js
@@ -1,5 +1,18 @@
 txt = "abc" ;
 
 for (char in txt) {
-    console.log(char)
+    // console.log(char);
+    // console.log(txt[char])
+    let character = txt[char]
+    list_vowels = ["a", "e", "i", "o", "u"]
+    // if (character == "a" ||
+    //     character == "e" ||
+    //     character == "i" ||
+    //     character == "o" ||
+    //     character == "u")
+    if (character in list_vowels) {
+        console.log("yes")
+    } else {
+        console.log(`${character} is not a vowel`)
+    }
 }
