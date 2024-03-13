14fa96ca67c3d69a6fe130ec9bb730d34d0705d4
n0m4official <ba55dragonofficial999@gmail.com>
2024-03-13 10:20:28 -0600

    testing

--- java/java.js
+++ java/java.js
@@ -5,14 +5,12 @@ for (char in txt) {
     // console.log(txt[char])
     let character = txt[char]
     list_vowels = ["a", "e", "i", "o", "u"]
-    // if (character == "a" ||
-    //     character == "e" ||
-    //     character == "i" ||
-    //     character == "o" ||
-    //     character == "u")
-    if (character in list_vowels) {
-        console.log("yes")
-    } else {
-        console.log(`${character} is not a vowel`)
+    for (vowel in list_vowels) {
+        if (vowel == char) {
+            console.log(`${character} is a vowel ${char}`)
+        } 
+        else {
+            console.log(`${character} is not a vowel`)
+        }
     }
 }
